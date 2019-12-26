f7aa0511b3edeebd713710e2047fb8833c21f4a5
Xercoy <corey@prak.me>
2019-12-25 23:41:03 -0800

    pki: added cfssl install tools

---
+++ pki/pki/cfssl-install-tools.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+# install tools required to generate and setup PKI Infrastructure
+
+curl -L https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 -o cfssl
+chmod +x cfssl
+
+curl -L https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 -o cfssljson
+chmod +x cfssljson
+
+curl -L https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 -o cfssl-certinfo
+chmod +x cfssl-certinfo
+
+mv cfssl cfssljson cfssl-certinfo /usr/local/bin
