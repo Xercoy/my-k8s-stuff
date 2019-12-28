804ec50cd259225710053f84480a9732a2170ece
Xercoy <corey@prak.me>
2019-12-27 17:09:31 -0800

    pki: tls script generation fixes

--- pki/generate-tls-certs.sh
+++ pki/generate-tls-certs.sh
@@ -2,13 +2,20 @@
 
 set -x
 # autogenerate TLS certs
+# a lot of this content is taken from Kubernetes the Hard Way
+
 
 # todp:
 # flexibility - assumptions of files are hardcoded
 # default checks for configuration files, etc
+# figure out what ${KUBERNETES_PUBLIC_ADDRESS} evalutates to - this is included as hostname for apiserver
 
-CSR_DEFAULT_COMPONENTS=("admin" "kublet" "kube-controller-manager" "kube-proxy" "kube-scheduler" "kube-api-server" "service-account")
+# Works on Ubuntu on Digital Ocean
+PUBLIC_IP_ADDR="$(hostname -I | awk -F ' ' '{print $1}')"
+
+CSR_DEFAULT_COMPONENTS=("admin" "kublet" "kube-controller-manager" "kube-proxy" "kube-scheduler" "kubernetes" "service-account")
 CA_CONFIG_PROFILE="kubernetes"
+KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
 
 COMPONENT_CUSTOM_COMMAND=""
 CSR_COMPONENT=""
@@ -62,7 +69,7 @@ gencert
 # in which requests for the kubelet might come from.
 # "CN": "system:node:${HOSTNAME}"
 # "O": "system:nodes"
-COMPONENT_CUSTOM_COMMAND="-hostname=${instance},${EXTERNAL_IP},${INTERNAL_IP}"
+COMPONENT_CUSTOM_COMMAND="-hostname=${HOSTNAME},${PUBLIC_IP_ADDR}"
 CSR_COMPONENT="kubelet"
 gencert
 
@@ -91,8 +98,9 @@ gencert
 # kube-api-server
 # "CN": "kubernetes"
 # "O": "Kubernetes"
-COMPONENT_CUSTOM_COMMAND="-hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES}"
-CSR_COMPONENT="kube-api-server"
+# need to get IP address
+COMPONENT_CUSTOM_COMMAND="-hostname=${HOSTNAME},${PUBLIC_IP_ADDR},10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,127.0.0.1,${KUBERNETES_HOSTNAMES}"
+CSR_COMPONENT= "kubernetes" # it's not "kube-api-server" - api-server and etcd will use this
 gencert
 
 # service account
