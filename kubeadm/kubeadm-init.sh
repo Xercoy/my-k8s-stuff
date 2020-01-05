b5f7875a25a787896a38b0aa732242e867c893a0
Corey Prak <corey@prak.me>
2020-01-04 22:28:46 -0800

    kubeadm: added restore flag to init script

--- kubeadm/kubeadm-init.sh
+++ kubeadm/kubeadm-init.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+MODE=$1
+
+KUBEADM_OPT=""
+KUBECTL_OPT=""
+
+if [[ "${MODE}" == "restore" ]]; then 
+    KUBEADM_OPT="--ignore-preflight-errors=DirAvailable--var-lib-etcd"
+fi
+
 # run kubeadm init with the right arguments
-kubeadm init --pod-network-cidr=192.168.0.0/16
+kubeadm init --pod-network-cidr=192.168.0.0/16 "${KUBEADM_OPT}"
 
 # setup kubeconfig
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config 
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# apply calico CRDs
-kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+# untaint master node
+
+if [[ "${MODE}" != "restore" ]]; then
+    # apply calico CRDs
+    kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+fi
