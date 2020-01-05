7fa189cf957a64cae5b477006712bf59ec5a0dd0
Corey Prak <corey@prak.me>
2020-01-04 23:49:18 -0800

    practice: added install script for single master node

---
+++ practice/general-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# this sets up a master node for each 
+
+# clone repo and change dirs to it
+git clone https://github.com/xercoy/my-k8s-stuff
+cd my-k8s-stuff/
+
+# install defaults and docker
+./general/setup.sh
+./general/install-docker.sh
+
+# install kubelet, kubeadm, and kubectl on the system
+./kubeadm/ubuntu-master-node-setup.sh
+
+# start cluster with kubeadm init, install calico, setup kubeconfig, etc
+kubeadm/kubeadm-init.sh
